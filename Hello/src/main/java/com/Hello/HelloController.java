acccb20f109c588c195fe6bbb29a64955d3a5c04
ashim <ashimhiras@gmail.com>
2025-05-18 21:19:48 +0530

    First commit

---
+++ Hello/src/main/java/com/Hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Ashim Lugun";
+    }
+}
