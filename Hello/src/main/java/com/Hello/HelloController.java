71bddf6be057a6e87cf6f08ae8a7703b1329f4cd
AshimLugun <156283569+AshimLugun@users.noreply.github.com>
2025-05-21 08:40:39 +0530

    Update HelloController.java

--- Hello/src/main/java/com/Hello/HelloController.java
+++ Hello/src/main/java/com/Hello/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/message")
     public String getMessage(){
-        return "Ashim Hiras Lugun";
+        return "Ashim Hiras Praful Lugun";
     }
 }
